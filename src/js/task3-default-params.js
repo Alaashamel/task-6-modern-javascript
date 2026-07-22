8f09c66d974a9d64fcf350a9314d88bcc07dc703
Alaashamel <Alaashamel32@gmail.com>
2026-07-22 19:27:48 +0300

    refactor: wrap Task 3 code in a function for better organization

--- src/js/task3-default-params.js
+++ src/js/task3-default-params.js
@@ -1,15 +1,22 @@
-// Task 3 - Default Parameter
+/**
+ * Task 3 - Default Parameter
+ * Demonstrates default parameter values in functions
+ */
 
 function printStudent(name, age = 20) {
     console.log(`Name: ${name}, Age: ${age}`);
 }
 
-console.log("=== Task 3: Default Parameter ===");
+function demonstrateDefaultParams() {
+    console.log("=== Task 3: Default Parameter ===");
 
-// Calling with only name (age will use default value of 20)
-printStudent("Ahmed");
+    // Calling with only name (age will use default value of 20)
+    printStudent("Ahmed");
 
-// Calling with both name and age
-printStudent("Ali", 25);
+    // Calling with both name and age
+    printStudent("Ali", 25);
 
-console.log("");
+    console.log("");
+}
+
+demonstrateDefaultParams();
