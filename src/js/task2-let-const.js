063c146f5221c5370d689483dd2f28d7ddc795fd
Alaashamel <Alaashamel32@gmail.com>
2026-07-22 19:27:21 +0300

    refactor: wrap Task 2 code in a function for better organization

--- src/js/task2-let-const.js
+++ src/js/task2-let-const.js
@@ -1,20 +1,27 @@
-// Task 2 - let & const
+/**
+ * Task 2 - let & const
+ * Demonstrates variable declarations in ES6+
+ */
 
-// Using let for variables that can be reassigned
-let name = "Ahmed";
+function demonstrateLetAndConst() {
+    // Using let for variables that can be reassigned
+    let name = "Ahmed";
 
-// Using const for constants that cannot be reassigned
-const PI = 3.14159;
+    // Using const for constants that cannot be reassigned
+    const PI = 3.14159;
 
-console.log("=== Task 2: let & const ===");
-console.log("Initial name:", name);
-console.log("PI:", PI);
+    console.log("=== Task 2: let & const ===");
+    console.log("Initial name:", name);
+    console.log("PI:", PI);
 
-// Changing the value of name (allowed with let)
-name = "Ali";
-console.log("Updated name:", name);
+    // Changing the value of name (allowed with let)
+    name = "Ali";
+    console.log("Updated name:", name);
 
-// Trying to reassign const will cause an error:
-// PI = 3.14;  // TypeError: Assignment to constant variable.
+    // Trying to reassign const will cause an error:
+    // PI = 3.14;  // TypeError: Assignment to constant variable.
 
-console.log("");
+    console.log("");
+}
+
+demonstrateLetAndConst();
