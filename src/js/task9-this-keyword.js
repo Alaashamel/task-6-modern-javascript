76fcfb416d3cf255671eee4596e0981a194d852d
Alaashamel <Alaashamel32@gmail.com>
2026-07-22 19:32:42 +0300

    refactor: wrap Task 9 code in a function for better organization

--- src/js/task9-this-keyword.js
+++ src/js/task9-this-keyword.js
@@ -1,16 +1,23 @@
-// Task 9 - this Keyword
+/**
+ * Task 9 - this Keyword
+ * Demonstrates the use of 'this' in object methods
+ */
 
-const employee = {
-    name: "Sara",
-    salary: 5000,
-    printInfo: function() {
-        console.log(`Name: ${this.name}`);
-        console.log(`Salary: ${this.salary}`);
-    }
-};
+function demonstrateThisKeyword() {
+    const employee = {
+        name: "Sara",
+        salary: 5000,
+        printInfo: function() {
+            console.log(`Name: ${this.name}`);
+            console.log(`Salary: ${this.salary}`);
+        }
+    };
 
-console.log("=== Task 9: this Keyword ===");
+    console.log("=== Task 9: this Keyword ===");
 
-// Calling the method that uses 'this'
-employee.printInfo();
-console.log("");
+    // Calling the method that uses 'this'
+    employee.printInfo();
+    console.log("");
+}
+
+demonstrateThisKeyword();
