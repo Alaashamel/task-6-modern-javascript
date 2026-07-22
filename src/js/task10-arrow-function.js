64349a1a0556a3ce68e71ea9f6137b6ec0ba59da
Alaashamel <Alaashamel32@gmail.com>
2026-07-22 19:33:29 +0300

    refactor: wrap Task 10 code in a function for better organization

--- src/js/task10-arrow-function.js
+++ src/js/task10-arrow-function.js
@@ -1,15 +1,22 @@
-// Task 10 - Arrow Function
+/**
+ * Task 10 - Arrow Function
+ * Demonstrates arrow function syntax vs regular functions
+ */
 
-// Regular function
-function sumRegular(x, y) {
-    return x + y;
+function demonstrateArrowFunction() {
+    // Regular function
+    function sumRegular(x, y) {
+        return x + y;
+    }
+
+    // Arrow function equivalent
+    const sum = (x, y) => x + y;
+
+    console.log("=== Task 10: Arrow Function ===");
+
+    console.log("Regular Function Result:", sumRegular(5, 3));
+    console.log("Arrow Function Result:", sum(5, 3));
+    console.log("");
 }
 
-// Arrow function equivalent
-const sum = (x, y) => x + y;
-
-console.log("=== Task 10: Arrow Function ===");
-
-console.log("Regular Function Result:", sumRegular(5, 3));
-console.log("Arrow Function Result:", sum(5, 3));
-console.log("");
+demonstrateArrowFunction();
